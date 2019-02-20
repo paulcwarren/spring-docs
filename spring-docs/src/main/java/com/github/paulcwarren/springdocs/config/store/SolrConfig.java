000d8f4d9fe53b805d7409cf890c0b4beb8f90df
Paul Warren <paul.warren@emc.com>
2019-02-20 12:49:27 -0800

    Enable embedded solr by default

--- spring-docs/src/main/java/com/github/paulcwarren/springdocs/config/store/SolrConfig.java
+++ spring-docs/src/main/java/com/github/paulcwarren/springdocs/config/store/SolrConfig.java
@@ -1,20 +1,9 @@
 package com.github.paulcwarren.springdocs.config.store;
 
-import org.springframework.content.solr.SolrProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-
 import com.github.paulcwarren.solr.EnableSolrEmbedded;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Profile("solr")
 @EnableSolrEmbedded
 public class SolrConfig {
-
-    public SolrProperties solrProperties() {
-        SolrProperties solrConfig =  new SolrProperties();
-        solrConfig.setUrl("http://localhost:983/solr/solr");
-        return solrConfig;
-    }
-
 }
