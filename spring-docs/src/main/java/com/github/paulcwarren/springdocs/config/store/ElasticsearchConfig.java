0ed0298b468691fb19d4a92bdb7641a6e95b3392
Paul Warren <paul.warren@emc.com>
2020-04-23 22:34:45 -0700

    Revert "Be explicit about enabling elasticsearch fulltext indexing"
    This reverts commit 8a3625edb43cb8dbdd7c864ef9065a54eb0c5ee2.

--- spring-docs/src/main/java/com/github/paulcwarren/springdocs/config/store/ElasticsearchConfig.java
+++ spring-docs/src/main/java/com/github/paulcwarren/springdocs/config/store/ElasticsearchConfig.java
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableElasticsearchFulltextIndexing
 public class ElasticsearchConfig {
 
     @Bean
